fc386aeed1631eeb19d3bfb0f0dc069c6d673fde
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-09-29 17:10:49 +0200

    Update code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { AppContainer } from 'react-hot-loader';
 
 import App from './containers/App';
 
+
+
 const render = Component => {
     ReactDOM.render(
         <AppContainer>
@@ -18,25 +20,3 @@ render(App);
 if (module.hot) {
     module.hot.accept('./containers/App', () => { render(App)})
 }
-
-
-
-
-/*
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
-
-
-if (module.hot) {
-    module.hot.accept('./containers/App', () => {
-        ReactDOM.render(
-                <AppContainer>
-                    <App />
-                </AppContainer>,
-        document.getElementById('app')
-        );
-    });
-}
-*/
